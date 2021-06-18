6a2883799f4a10cc73d6cf18639b12765dc48c13
kshintani3 <kousuke.shintani.0609@gmail.com>
2021-06-18 19:29:01 +0900

    modify .ignore

--- template/algorithm.py
+++
@@ -1,6 +0,0 @@
-import sys
-sys.setrecursionlimit(10000)
-
-# 二分探索
-bisect(list, value)
-
--- template/output.py
+++
@@ -1,6 +0,0 @@
-print('alpha', end=' ')
-print('bravo', end=' ')
-#alpha bravo
-
-print(' '.join(map(str, e)))
-#-8 -6 -9 120 -97
