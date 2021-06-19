8fb95a05e84037d6a81c3ae7e6e20d93c57ff939
kshintani3 <kousuke.shintani.0609@gmail.com>
2021-06-19 19:02:47 +0900

    add another code

--- ABC/ABC_205/D_Kth Excluded_2.py
+++ ABC/ABC_205/D_Kth Excluded_2.py
@@ -1,1 +1,32 @@
+# 16/21でTLE
 
+def search(b, a):
+    a_len = (len(a) + 1) // 2
+    if len(a) == 1:
+        return a[0]
+    elif a[0] > b:
+        return a[0]
+    elif a[-1] < b:
+        return a[-1]
+    elif a[a_len] >= b:
+        return search(b, a[0:a_len])
+    else:
+        return search(b, a[a_len:len(a)])
+
+
+n, q = map(int, input().split())
+a = list(map(int, input().split()))
+a_list = [0]
+for i in range(len(a)):
+    a_list.append(a[i] - i - 1)
+a.insert(0, 0)
+
+for i in range(q):
+    query = int(input())
+    ind = search(query, a_list)
+    ind_list = [i for i, x in enumerate(a_list) if x == ind]
+    ind = ind_list[-1]
+    if ind == 0:
+        print(query)
+    else:
+        print(a[ind] + query - a_list[ind])
