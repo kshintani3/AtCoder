dac9b2a240c5904772683d985f39ec185b2b13f5
kshintani3 <doragons0624@gmail.com>
2021-08-21 22:53:20 +0900

    add ABC_213

---
+++ ABC/ABC_215/C_One More aab aba baa.py
@@ -0,0 +1,15 @@
+import itertools
+
+S, K = map(str, input().split())
+K = int(K)
+S_list = sorted(list(S))
+
+comb_list = list(set(itertools.permutations(S_list)))
+
+ans_list = []
+for i in comb_list:
+    ans_list.append("".join(i))
+
+ans_list.sort()
+
+print(ans_list[K - 1])
