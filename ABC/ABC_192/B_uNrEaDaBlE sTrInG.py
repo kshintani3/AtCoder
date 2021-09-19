aa820a74746bb9145d8d3f67c0db5c43abf28f50
kshintani3 <doragons0624@gmail.com>
2021-09-19 21:15:07 +0900

    modify file

--- ABC/ABC_192/B_uNrEaDaBlE sTrInG.py
+++ ABC/ABC_192/B_uNrEaDaBlE sTrInG.py
@@ -2,6 +2,7 @@ S = input()
 lo_list = [S[i] for i in range(len(S)) if i % 2 == 0]
 up_list = [S[i] for i in range(len(S)) if i % 2 == 1]
 
+# listの要素が0である時の対処
 lo_list.append("a")
 up_list.append("A")
 
