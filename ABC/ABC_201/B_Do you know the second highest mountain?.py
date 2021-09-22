c644bdd4cb303211572a1864400182dd5560babb
kshintani3 <doragons0624@gmail.com>
2021-09-22 23:41:38 +0900

    add file

--- ABC/ABC_201/B_Do you know the second highest mountain?.py
+++ ABC/ABC_201/B_Do you know the second highest mountain?.py
@@ -1,7 +1,9 @@
-n = int(input())
-list = []
-for i in range(n):
-    a, b = input().split()
-    list.append((a, int(b)))
-d = sorted(list, key=lambda x: (x[1]), reverse=True)
+N = int(input())
+y_list = []
+
+for i in range(N):
+    s, t = input().split()
+    y_list.append((s, int(t)))
+
+d = sorted(y_list, key=lambda x: (x[1]), reverse=True)
 print(d[1][0])
--- ABC/ABC_201/README.md
+++ ABC/ABC_201/README.md
@@ -1,1 +1,4 @@
 # AtCoder Beginner Contest 201
+## B - Do you know the second highest mountain?
+- `sorted(y, key=lambda x:(x[1]), reverse=True)`
+ : list`y`を2番目の値を基準にソートする。`reverse=True`の時は大きい順
