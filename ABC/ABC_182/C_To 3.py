6d87e6f0eeab3565950d90cec14002843712711b
kshintani3 <doragons0624@gmail.com>
2021-09-24 23:48:59 +0900

    modify file

--- ABC/ABC_182/C_To 3.py
+++ ABC/ABC_182/C_To 3.py
@@ -1,6 +1,12 @@
+import itertools
+
 N = input()
 n = [int(i) for i in N]
 len_n = len(n)
 
 for i in range(len_n):
-
+    for j in itertools.combinations(n, len_n - i):
+        if sum(j) % 3 == 0:
+            print(i)
+            exit()
+print(-1)
