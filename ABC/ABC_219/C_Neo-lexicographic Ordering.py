cea8af9cb45d41b69cae3972cabdf9407d4f5b8a
kshintani3 <doragons0624@gmail.com>
2021-10-04 23:12:24 +0900

    modify ABC_219

--- ABC/ABC_219/C_Neo-lexicographic Ordering.py
+++ ABC/ABC_219/C_Neo-lexicographic Ordering.py
@@ -4,13 +4,11 @@ ans_list = {}
 
 for _ in range(N):
     S = input()
-    S_len = len(S)
-    cnt = 0
-    for i in range(S_len):
-        cnt += X.index(S[i]) + 26 * (S_len - i - 1)
+    cnt = ""
+    for i in S:
+        cnt += chr(X.index(i) + 97)
     ans_list[cnt] = S
 
-S_cnt = sorted(ans_list.keys(), reverse=True)
-print(ans_list)
-for i in S_cnt:
+ans_ind = sorted(ans_list.keys())
+for i in ans_ind:
     print(ans_list[i])
