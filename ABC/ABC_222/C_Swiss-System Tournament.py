60adeef2b8669cca1bfa565c3b5851be321ef70a
kshintani3 <doragons0624@gmail.com>
2021-10-15 00:03:50 +0900

    add ABC_222_C

--- ABC/ABC_222/C_Swiss-System Tournament.py
+++ ABC/ABC_222/C_Swiss-System Tournament.py
@@ -1,11 +1,16 @@
 N, M = map(int, input().split())
-a = [list(map(int, list(input()))) for i in range(N)]
-rank = [range(N)]
-# じゃんけんの組み合わせ９パターン
-judge = {"GG": (1, 1), }
-# 試合後に何点入ったか記録して、valueでソートした順番をrankに格納
-wl = {}
+a = [input() for i in range(2 * N)]
+
+win_cnt = [0] * (2 * N)
+rank = list(range(2 * N))
 
 for i in range(M):
-    for j in range(0, N, 2):
-        game = a[j][i] + a[j + 1][i]
+    for j in range(0, 2 * N - 1, 2):
+        L, R = a[rank[j]][i], a[rank[j + 1]][i]
+        if L + R in ["GC", "CP", "PG"]:
+            win_cnt[rank[j]] += 1
+        elif L + R in ["CG", "PC", "GP"]:
+            win_cnt[rank[j + 1]] += 1
+    rank = sorted(rank, key=lambda x: (-win_cnt[x], x))
+for ans in rank:
+    print(ans + 1)
