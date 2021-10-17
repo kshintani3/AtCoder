2388493a4b504c7fbfb7dd6cd1bc1ec9b743eb1f
kshintani3 <doragons0624@gmail.com>
2021-10-17 23:09:23 +0900

    modify ABC_223_C

--- ABC/ABC_223/C_Doukasen.py
+++ ABC/ABC_223/C_Doukasen.py
@@ -13,9 +13,9 @@ sum_c = sum(c_list)
 # 左の導火線から所要時間の合計を計算
 for i in range(len(c_list)):
     le += c_list[i]
-    # 所要時間の合計の半分を超える
+    # 合計の所要時間の半分を超える
     if sum_c / 2 <= le:
-        pre_le = le - c_list[i]  # 一つ前までの所要時間
+        pre_le = le - c_list[i]  # 一つ前までの所要時間の合計
         ri = sum(c_list[i + 1:])  # 右の火がどれくらいの時間燃えているか
         sa = abs(ri - pre_le) * b_list[i]  # 先に導火線iについた火がどれくらい進んでいるか
 
