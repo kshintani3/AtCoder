db8bba4e4ab26068c22d0fb0983cc4c558cddd77
kshintani3 <doragons0624@gmail.com>
2022-02-13 18:59:09 +0900

    add ABC_107_B

--- ABC/ABC_107/B_Grid Compression.py
+++ ABC/ABC_107/B_Grid Compression.py
@@ -4,19 +4,12 @@ ans = []
 
 for _ in range(H):
     ai = input()
-    if ai.count("#") != 0:
+    if "#" in ai:
         a.append(ai)
 
-for j in range(W):
-    flag = False
-    for i in range(len(a)):
-        if a[i][j] == "#":
-            flag = True
-            break
-    if flag:
+for j in zip(*a):
+    if "#" in j:
         ans.append(j)
 
-for out in a:
-    for i in ans:
-        print(out[i], end="")
-    print()
+for i in zip(*ans):
+    print("".join(i))
