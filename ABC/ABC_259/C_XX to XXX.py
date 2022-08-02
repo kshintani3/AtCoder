44348bdef600ef66432fdbcc193d02230f367b56
kshintani3 <doragons0624@gmail.com>
2022-08-02 23:04:16 +0900

    modify ABC_259_C

--- ABC/ABC_259/C_XX to XXX.py
+++ ABC/ABC_259/C_XX to XXX.py
@@ -1,14 +1,14 @@
 S = input()
 T = input()
 
-i, j, ti = 0, 0, ''
+i, j, x = 0, 0, ''
 while i < len(T):
     sc, tc = 0, 0
-    ti = T[i]
-    while i != len(T) and ti == T[i]:
+    x = T[i]
+    while i != len(T) and x == T[i]:
         tc += 1
         i += 1
-    while j != len(S) and ti == S[j]:
+    while j != len(S) and x == S[j]:
         sc += 1
         j += 1
     if (sc == 1 and tc == 1) or 1 < sc <= tc:
